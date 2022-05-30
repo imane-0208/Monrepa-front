43b33374d9a30d0e888b25e04aa3b8cb9ad042f9
imane <rkhisimane02@gmail.com>
2022-05-30 11:05:55 +0100

    monrepa-front

---
+++ run.sh
@@ -0,0 +1,6 @@
+code .
+npx kill-port 19000
+npx kill-port 19002
+start expo start
+start chrome.exe http://localhost:19002/
+start chrome.exe http://localhost:19006/
---
+++ src/screens/Home.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState, useRef } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Text,
+  Button,
+  FlatList,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import Header from "../components/HeaderHome";
+import Categorie from "../components/Categorie";
+import CardHome from "../components/HomeCards";
+import Global from "../style/Global";
+import Produits from "../ressources/Produits";
+import tw from "twrnc"
+
+const Home = () => {
+  const [Food, setFood] = useState([]);
+
+  const getAllFood = () => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:5000/api/recette/getAll", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        setFood(result.Recettes);
+        console.log("rece", result.Recettes);
+      })
+      .catch((error) => console.log("error", error));
+  };
+
+  useEffect(() => {
+    getAllFood();
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Header />
+        <Categorie />
+        <View style={Global.Categorie}>
+          <View style={Global.CategorieTitle}>
+            <Text style={Global.CategorieTitle}>Meilleurs Produits</Text>
+            <TouchableOpacity>
+              {/* <AntDesign name="arrowright" size={24} color="black" /> */}
+            </TouchableOpacity>
+          </View>
+          <ScrollView style={tw`flex-1 max-h-[300px] `}>
+            {Food?.map((Produit, index) => (
+              <CardHome key={index} {...{ ...Produit, Food }} />
+            ))}
+          </ScrollView>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
